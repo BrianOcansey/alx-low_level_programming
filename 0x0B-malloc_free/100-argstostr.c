b8b102a6098d20bc6c4026258e931cc56654cc27
Brian Ocansey <brianocansey@yahoo.com>
2022-10-05 04:48:58 -0700

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,83 @@
 #include "main.h"
+
 #include <stdlib.h>
+
 /**
-* argstostr - concatenates all the arguments of your program.
-* @ac: number of arguments
-* @av: double pointer to arguments
-*
-* Return:pointer to new string, or NULL if error
-*/
+
+  *argstostr - concatenates all arguments of the program.
+
+  *@ac: argument count.
+
+  *@av: pointer to array of size ac.
+
+  *Return: NULL if ac == 0 or av == null, Pointer to new string.
+
+  *NULL on fail.
+
+  */
+
 char *argstostr(int ac, char **av)
+
 {
-int i, j, k = 0, n = 0;
-char *s;
-if (ac <= 0 || av == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-n++;
-n++;
-}
-n++;
-s = malloc(n * sizeof(char));
-if (s == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-s[k] = av[i][j];
-k++;
-}
-s[k] = '\n';
-k++;
-}
-s[k] = '\0';
-return (s);
+
+	int i, n, k = 0, len = 0;
+
+	char *str;
+
+
+
+	if (ac == 0 || av == NULL)
+
+		return (NULL);
+
+
+
+	for (i = 0; i < ac; i++)
+
+	{
+
+		for (n = 0; av[i][n]; n++)
+
+			len++;
+
+	}
+
+	len += ac;
+
+
+
+	str = malloc(sizeof(char) * len + 1);
+
+	if (str == NULL)
+
+		return (NULL);
+
+
+
+	for (i = 0; i < ac; i++)
+
+	{
+
+		for (n = 0; av[i][n]; n++)
+
+		{
+
+			str[k] = av[i][n];
+
+			k++;
+
+		}
+
+		if (str[k] == '\0')
+
+		{
+
+			str[k++] = '\n';
+
+		}
+
+	}
+
+	return (str);
+
 }
