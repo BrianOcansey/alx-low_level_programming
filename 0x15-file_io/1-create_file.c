dbbeee133a027ea6ed65b886e136ab813ca62822
Brian Ocansey <brianocansey@yahoo.com>
2022-10-31 18:24:34 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,67 @@
 #include "main.h"
-#include <stddef.h>
+
+
+
 /**
-* _strlen - counts string length
-* @str: string to be used
-*
-* Return: length of the string
-*/
-int _strlen(char *str)
-{
-int len = 0;
-while (str[len] != '\0')
-len++;
-return (len);
-}
-/**
-* create_file - creates a file
-* @filename: name of the file
-* @text_content: content of the file to be created
-*
-* Return: 1 on success, -1 otherwise
-*/
+
+ * create_file - Creates a file.
+
+ * @filename: A pointer to the name of the file to create.
+
+ * @text_content: A pointer to a string to write to the file.
+
+ *
+
+ * Return: If the function fails - -1.
+
+ *         Otherwise - 1.
+
+ */
+
+
+
 int create_file(const char *filename, char *text_content)
+
 {
-int file, wrote;
-if (filename == NULL)
-return (-1);
-file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (file == -1)
-return (-1);
-if (text_content != NULL)
-{
-wrote = write(file, text_content, _strlen(text_content));
-if (wrote == -1)
-{
-close(file);
-return (-1);
-}
-close(file);
-return (1);
-}
-else
-{
-close(file);
-return (1);
-}
+
+	int o, w, len = 0;
+
+
+
+	if (filename == NULL)
+
+		return (-1);
+
+
+
+	if (text_content != NULL)
+
+	{
+
+		for (len = 0; text_content[len];)
+
+			len++;
+
+	}
+
+
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	w = write(o, text_content, len);
+
+
+
+	if (o == -1 || w == -1)
+
+		return (-1);
+
+
+
+	close(o);
+
+
+
+	return (1);
+
 }
